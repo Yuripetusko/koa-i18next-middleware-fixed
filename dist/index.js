c5f72ba5417cf32997dc821145024168efdcc581
yuripetusko <yuripetusko@gmail.com>
2020-08-17 12:22:45 +0100

    use ctx.req to pass i18n instance to be able to utilize it on nextjs

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,17 @@
-'use strict';
+'use strict';Object.defineProperty(exports, "__esModule", { value: true });exports.LanguageDetector = undefined;exports.
 
-import * as utils from './utils';
-import LD from 'koa-i18next-detector';
 
-export var LanguageDetector = LD;
 
-export function getHandler(i18next, options = {}) {
-    return async function(ctx, next) {
+
+
+
+getHandler = getHandler;var _utils = require('./utils');var utils = _interopRequireWildcard(_utils);var _koaI18nextDetector = require('koa-i18next-detector');var _koaI18nextDetector2 = _interopRequireDefault(_koaI18nextDetector);function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}function _interopRequireWildcard(obj) {if (obj && obj.__esModule) {return obj;} else {var newObj = {};if (obj != null) {for (var key in obj) {if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key];}}newObj.default = obj;return newObj;}}var LanguageDetector = exports.LanguageDetector = _koaI18nextDetector2.default;function getHandler(i18next, options = {}) {
+    return async function (ctx, next) {
         let res = ctx.res;
         let req = ctx.req;
         let ignores =
-            (options.ignoreRoutes instanceof Array && options.ignoreRoutes) ||
-            [];
+        options.ignoreRoutes instanceof Array && options.ignoreRoutes ||
+        [];
         if (ignores.some(ignore => ctx.path.includes(ignore))) {
             return await next();
         }
@@ -22,8 +22,8 @@ export function getHandler(i18next, options = {}) {
             req.locale = lng;
             req.lng = lng;
             req.languages = i18next.services.languageUtils.toResolveHierarchy(
-                lng
-            );
+            lng);
+
         });
 
         let lng = req.lng;
@@ -42,9 +42,9 @@ export function getHandler(i18next, options = {}) {
 
         if (req.i18nextLookupName === 'path' && options.removeLngFromUrl) {
             req.url = utils.removeLngFromUrl(
-                req.url,
-                i18next.services.languageDetector.options.lookupFromPathIndex
-            );
+            req.url,
+            i18next.services.languageDetector.options.lookupFromPathIndex);
+
         }
 
         let t = i18n.t.bind(i18n);
@@ -78,8 +78,7 @@ export function getHandler(i18next, options = {}) {
             await next();
         }
     };
-}
+}exports.default =
 
-export default {
-    getHandler
-};
+{
+    getHandler };
